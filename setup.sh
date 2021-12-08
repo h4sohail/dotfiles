f7a4734f491ff7dc3267244b703458d0921653e6
Hamza <hamza.sohail@carleton.ca>
2021-12-08 17:35:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@ if [ "$SPIN" ]; then
   cat > ~/.bash_aliases  <<- "EOF"
   alias STOP_PROC='systemctl stop proc-shopify--shopify@server.service && systemctl stop proc-shopify--shopify@worker.service'
   alias START_PROC='systemctl start proc-shopify--shopify@server.service && systemctl start proc-shopify--shopify@worker.service'
+  alias start=/usr/bin/start
+  alias stop=/usr/bin/stop
+  alias attach=/usr/bin/attach
   EOF
 fi
